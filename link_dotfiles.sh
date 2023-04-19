24dc7dc917c8adc23c5825a60a348ffd493fe104
Til Roth <s8tiroth@stud.uni-saarland.de>
2023-04-19 19:41:58 +0200

    add make script.

---
+++ link_dotfiles.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+ln -sn ~/dotfiles/nvim-config/ ~/.config/nvim
+ln -sn ~/dotfiles/alacritty/ ~/.config/alacritty
+ln -sn ~/dotfiles/qutebrowser/ ~/.config/qutebrowser
+ln -s ~/dotfiles/gitignore ~/.gitignore
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/zshrc ~/.zshrc
