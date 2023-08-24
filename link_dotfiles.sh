404ccef65210e5be60a61a44d840a1a3790be419
Til Roth <s8tiroth@stud.uni-saarland.de>
2023-08-24 15:56:35 +0200

    add zathurarc

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -3,6 +3,7 @@
 ln -sn ~/dotfiles/nvim-config/ ~/.config/nvim
 ln -sn ~/dotfiles/alacritty/ ~/.config/alacritty
 ln -sn ~/dotfiles/qutebrowser/ ~/.config/qutebrowser
+ln -sn ~/dotfiles/zathura/ ~/.config/zathura
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/zshrc ~/.zshrc
